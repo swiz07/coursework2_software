55134080bca7013882136dc886386e8cb86a65a2
swiz07 <swizeldemelo@gmail.com>
2025-03-27 18:07:03 +0000

    Add URL pattern for engineer home page

--- health_check/urls.py
+++ health_check/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth import views as log_views
 urlpatterns = [
     path('', views.home, name='home'),  # Home page
     path('register/', views.register, name='register'),  # Register URL
-    path('login/', log_views.LoginView.as_view(template_name='health_check/login.html'), name='login'),
+     path('engineer/', views.enghome, name='enghome'),  # Engineer page
+    path('login/', log_views.LoginView.as_view(template_name='health_check/login.html'), name='login'), 
     path('logout/', log_views.LogoutView.as_view(template_name='health_check/logout.html'), name='logout'),
 ]
