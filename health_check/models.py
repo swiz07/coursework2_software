29e0dfc6b2d837841f36081c2e74175e92f0e568
swiz07 <swizeldemelo@gmail.com>
2025-03-29 00:16:24 +0000

    Updated manage.py file

--- health_check/models.py
+++ health_check/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+#class User(AbstractUser):
+ #   email=models.EmailField(max_length=254, unique=True)
+ #   fullname=models.CharField(max_length=254, null=True, blank=True)
+ #   address=models.TextField()
+ #   phonenumber=models.TextField(max_length=15)
+
